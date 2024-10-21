846ea2ed8cb54bb8589de411bb61f7e1556e5ad2
titandewa <ruci250@gmail.com>
2024-10-21 10:17:24 +0700

    dewa 3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+ public static void main(String[] args) {
+ String text = "Hello, Maven!";
+ String reversedText = StringUtils.reverse(text);
+ System.out.println(reversedText);
+ }
+ }
